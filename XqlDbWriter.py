e71fe8f1dd3b68386db636257a516e5d9d694f00
tom <tomtalp93@gmail.com>
2015-03-28 23:24:40 +0300

    Began work on the DBWriter

---
+++ XqlDbWriter.py
@@ -0,0 +1,25 @@
+##########################################
+# XQL
+# Create an SQLite DB from the Excel file
+##########################################
+
+import sys
+import sqlite3
+import parse_xls
+
+class DBWriter(object):
+	def __init__(self, file_path):
+		self.XQL_db_object = parse_xls.parse_xls_to_db(file_path)
+		self.db_location = None
+
+	def create_db(self):
+		"""
+		Create the SQLite DB file
+		"""
+		pass
+		#Modify self.db_location
+
+		#TODO - Decide whether we write in cwd or /tmp/
+
+	def write_to_db(self):
+		db_conn = sqlite3.connect(self.db_location)
