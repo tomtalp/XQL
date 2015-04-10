f61cbe03b93844f6995a918b998ba93642b87279
sid802 <sid802@users.noreply.github.com>
2015-04-10 13:25:24 +0300

    Turns dates into datetime objects, and converts necessary cell values into what they should be

--- XqlParser.py
+++ XqlParser.py
@@ -8,7 +8,7 @@
 ################################################################################################
 from distutils.command.build_scripts import first_line_re
 
-import xlrd, re, os, random
+import xlrd, re, os, random, datetime
 
 
 ####### Classes #######
@@ -35,7 +35,7 @@ class XqlTable(object):
 
     def add_header(self, header_name, header_type):
         #replaces all non word characters (letters, numbers and underscore) into underscores
-        filtered_header_name = filter_header(header_name)
+        filtered_header_name = filter_name(header_name)
 
         if filtered_header_name in self.headers.keys():
             suffix = 1
@@ -57,12 +57,12 @@ class XqlTable(object):
 
 ####### Parsing #######
 
-def filter_header(header_name):
+def filter_name(header_name):
     return re.sub('\W', '_', header_name).upper()
 
 def parse_xls_to_db(xls_path, rows_per_iter):
     file_name = os.path.splitext(os.path.basename(xls_path))[0]
-    parsed_db = XqlDB(file_name)
+    parsed_db = XqlDB(filter_name(file_name))
     source_workbook = xlrd.open_workbook(xls_path)
 
     #Parse each sheet in the xls file
@@ -79,29 +79,26 @@ def parse_sheet_to_table(workbook, sheet, rows_per_iter):
 
     last_row = sheet.nrows - 1
     last_col = sheet.ncols - 1
-    date_cols = []
 
     #minimum 1 rows (only header)
     if last_col >= 0:
 
-        table = XqlTable(sheet.name)
+        table = XqlTable(filter_name(sheet.name))
 
         #If we want to find where the table actually starts (might not start at 0, 0),
         #we have to start from the last filled cell and go back until we get to the first cell
         first_row, first_col = find_first_cell(sheet, last_row, last_col)
 
         #Add headers
-        column_nums = []
+        column_nums = [] #index = column num, value = (column_name, column_type)
         for col in xrange(first_col, last_col + 1):
             header_name = sheet.cell_value(first_row, col)
             header_xlrd_type = get_column_xlrd_type(sheet, col, first_row, last_row)
             header_sqlite_type = xlrd_type_to_sqlite_type(header_xlrd_type)
-            if header_sqlite_type == 'DATETIME':
-                date_cols.append(col)
             filtered_header_name = table.add_header(header_name, header_sqlite_type)
-            column_nums.append(filtered_header_name)
+            column_nums.append((filtered_header_name, header_sqlite_type))
 
-        table.gen_rows = generate_sheet_rows(workbook, sheet, first_row, last_row, first_col, last_col, column_nums, date_cols, rows_per_iter)
+        table.gen_rows = generate_sheet_rows(workbook, sheet, first_row, last_row, first_col, last_col, column_nums, rows_per_iter)
         return table
 
     return False
@@ -133,7 +130,7 @@ def find_first_col(sheet, first_row, last_col):
             return col
 
 
-def generate_sheet_rows(workbook, sheet, first_row, last_row, first_col, last_col, table_headers, date_cols, rows_per_iter = 5):
+def generate_sheet_rows(workbook, sheet, first_row, last_row, first_col, last_col, table_headers, rows_per_iter = 5):
     """
 
     returns a generator and yields a dictionary having the cell_value as value and header_name as key
@@ -151,14 +148,18 @@ def generate_sheet_rows(workbook, sheet, first_row, last_row, first_col, last_co
         while row < origin_row + rows_per_iter and row <= last_row:
             row_values = {}
             for col in xrange(first_col, last_col + 1):
-                header_name = table_headers[col]
-
+                header_name = table_headers[col][0]
                 value = sheet.cell_value(row, col)
 
-                #If it's a date, convert it into a datetime tuple
-                if col in date_cols:
-                    if sheet.cell_type(row, col) == 3:
-                        value = xlrd.xldate_as_tuple(value, workbook.datemode)
+                src_type = xlrd_type_to_sqlite_type(sheet.cell_type(row, col))
+                target_type = table_headers[col][1]
+
+                if src_type == target_type == 'DATETIME':
+                    year, month, date, hour, minute, second = xlrd.xldate_as_tuple(value, workbook.datemode)
+                    value = datetime.datetime(year, month, date, hour, minute, second)
+                if src_type != target_type:
+                    value = convert_cell_type(value, src_type, target_type, workbook.datemode)
+
                 row_values[header_name] = value
             rows_block.append(row_values)
             row += 1
@@ -206,15 +207,43 @@ def get_column_xlrd_type(sheet, col, first_row, last_row):
         #No 2 cells have different values
         return cell_value
 
+def convert_cell_type(value, src_type, target_type, datemode):
+    """
+
+    gets sqlite source and target type, and converts the value if necessary
+    This func is only called when src_type and target_type are different
+
+    Dates that must be converted to string are converted into the following format: %d-%m-%Y %H:%M
+
+    """
+    if target_type == 'VARCHAR':
+        if src_type == 'DATETIME':
+            year, month, date, hour, minute, second = xlrd.xldate_as_tuple(value, datemode)
+            python_date = datetime.datetime(year, month, date, hour, minute, second)
+            value = python_date.strftime('%d-%m-%Y %H:%M')
+        elif src_type in ('FLOAT', 'BOOLEAN'):
+            value = str(value)
+    elif target_type == 'FLOAT':
+        if src_type == 'BOOLEAN':
+            value = float(True)
+        elif src_type == 'VARCHAR':
+            value = 0 # default float
+        elif src_type == 'DATETIME': # Dates are stored as float numbers
+            pass
+    elif target_type == 'BOOLEAN':
+        value = bool(value)
+    elif target_type == 'DATETIME':
+        value = None
+    return value
+
 
 ####### End Parsing ###### end
 
 def main():
-    if __name__ == '__main__':
+    xls_path = raw_input("Enter xls path:\n")
+    while not (os.path.isfile(xls_path) and os.path.splitext(xls_path)[1] in ('.xls', '.xlsx')):
         xls_path = raw_input("Enter xls path:\n")
-        while not (os.path.isfile(xls_path) and os.path.splitext(xls_path)[1] in ('.xls', '.xlsx')):
-            xls_path = raw_input("Enter xls path:\n")
+    rows_per_iter = input("Enter number of rows you want the generator to return:\n")
+    while not isinstance(rows_per_iter, int):
         rows_per_iter = input("Enter number of rows you want the generator to return:\n")
-        while not isinstance(rows_per_iter, int):
-            rows_per_iter = input("Enter number of rows you want the generator to return:\n")
-        return parse_xls_to_db(xls_path, rows_per_iter)
+    return parse_xls_to_db(xls_path, rows_per_iter)
